9ea3d6ea511570a2c9137715daaa4bf5a44fcb4e
BrunoVilela <brunovilelasilva@hotmail.com>
2016-08-18 15:06:52 -0500

    Add other functions

--- R/FARM-package.R
+++ R/FARM-package.R
@@ -1,5 +1,7 @@
 #' @importFrom Rcpp evalCpp
 #' @importFrom ape reorder.phylo
-#' @export NewTip DropTip TheOriginOfSpecies makePhy
+#' @export NewTip DropTip TheOriginOfSpecies makePhy Arisal parameters getTargets
+#' @export extinct BuildWorld Diffusion Extinction RunSim speciate Speciation SpeciationTakeOver
+#' @export TakeOver sub.TakeOver RunSimUltimate
 #' @useDynLib FARM
 NULL
