c3826d349096a434f713ecd7b7b6b52f7de381d3
BrunoVilela <brunovilelasilva@hotmail.com>
2016-08-19 12:18:52 -0500

    Add combo

--- R/FARM-package.R
+++ R/FARM-package.R
@@ -2,6 +2,6 @@
 #' @importFrom ape reorder.phylo
 #' @export NewTip DropTip TheOriginOfSpecies makePhy Arisal parameters getTargets
 #' @export extinct BuildWorld Diffusion Extinction RunSim speciate Speciation SpeciationTakeOver
-#' @export TakeOver sub.TakeOver RunSimUltimate RunSim2 RunSimUltimate2
+#' @export TakeOver sub.TakeOver RunSimUltimate RunSim2 RunSimUltimate2 combo_of_choice
 #' @useDynLib FARM
 NULL
