2ad9644cdfb83690039e4d4d1f09d191a454253e
BrunoVilela <brunovilelasilva@hotmail.com>
2016-10-18 12:48:51 -0500

    Adding new coords

--- R/FARM-package.R
+++ R/FARM-package.R
@@ -7,9 +7,10 @@
 #' @importFrom utils head
 #' @import picante
 #' @import ape
+#' @import spdep
 #' @export NewTip DropTip TheOriginOfSpecies makePhy Arisal parameters getTargets
 #' @export extinct BuildWorld Diffusion Extinction RunSim speciate Speciation SpeciationTakeOver
 #' @export TakeOver sub.TakeOver RunSimUltimate RunSim2 RunSimUltimate2 combo_of_choice evol.distinct2
-#' @export DivDep Dsig bd Module_2
+#' @export DivDep Dsig bd Module_2 coords.bantu coords.austronisian coords.uto
 #' @useDynLib FARM
 NULL
