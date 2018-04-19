a9061630b1d27678aafefacfe71f5dd12dd65103
BrunoVilela <brunovilelasilva@hotmail.com>
2018-04-19 15:21:45 -0500

    new changes serial analysis, div depent sampling percentage, and scale tree.

--- R/Serial_result_analysis.R
+++ R/Serial_result_analysis.R
@@ -12,6 +12,9 @@ Module_2 <- function(Module_1_output) {
   } else {
 
     this_tree <- Module_1_output$mytree
+    this_tree$edge.length <-
+      this_tree$edge.length/max(phytools::nodeHeights(this_tree)[,2]) * scale
+
     this_world <- Module_1_output$myWorld
 
 
